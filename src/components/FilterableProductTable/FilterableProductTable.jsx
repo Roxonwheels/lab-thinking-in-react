3b280b61774f6274190a623321fbe3ca58996d30
Rox <you@example.com>
2021-12-05 01:12:16 +0100

    components

---
+++ src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './data.json'
+
+const FilterableProductTable  = () =>{
+
+    return(
+        <div className="FilterableProductTable "> 
+
+        </div>
+    )
+}
